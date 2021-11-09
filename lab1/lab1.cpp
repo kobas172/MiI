b2f7375907d8df9524db76c28f5a2d617391f5ec
kobas <60666561+kobas172@users.noreply.github.com>
2021-11-09 20:23:44 +0100

    Add files via upload

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -1,15 +1,96 @@
 // lab1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <fstream>
+#include <string>
 
 using namespace cv;
+using namespace std;
 
 
 int main()
 {
     Mat srcImage;
+    string line;
+    string lines[9];
+    int i = 0;
+
+    //Rysowanie kwadratu na kazdym zdjeciu
+    //char letter = 'A';
+
+    //for (int i = 0; i < 10; i++)
+    //{
+    //    std::string dir = "Images\\";
+    //    std::string dirOut = dir;
+    //    dir += letter;
+    //    dir += ".jpg";
+    //    dirOut += letter;
+    //    dirOut += "_OUT.jpg";
+    //    srcImage = imread(dir);
+    //    if (!srcImage.data) {
+    //        std::cout << "Nie mozna wczytac obrazu!" << std::endl;
+    //        return 1;
+    //    }
+    //    rectangle(srcImage, Rect(100, 100, 100, 100), Scalar(0, 0, 0), -1, 1, 0);
+    //    imwrite(dirOut, srcImage);
+    //    letter = (int)letter + 1;
+    //    srcImage.release();
+    //}
+
+    ifstream myfile("184515.csv");
+    if (myfile.is_open())
+    {
+        while (getline(myfile, line))
+        {
+            if (i > 0 && i < 10)
+            {
+                lines[i - 1] = line;
+            }
+            i++;
+        }
+        myfile.close();
+    }
+
+    char letter = 'A';
+    char* first = NULL;
+    char* second = NULL;
+    for (int i = 0; i < 9; i++)
+    {
+        string dir = "Images\\";
+        string dirOut = dir;
+        dir += letter;
+        dir += ".jpg";
+        dirOut += letter;
+        dirOut += "_OUT.jpg";
+
+        srcImage = imread(dir);
+        if (!srcImage.data) {
+            std::cout << "Nie mozna wczytac obrazu!" << std::endl;
+            return 1;
+        }
+
+        int n = lines[i].length();
+        char linesChar[1000];
+        strcpy(linesChar, lines[i].c_str());
+
+        first = strtok(linesChar, ";");
+        while (true) {
+            first = strtok(NULL, ";");
+            second = strtok(NULL, ";");
+            if (first == NULL && second == NULL)
+            {
+                break;
+            }
+            circle(srcImage, Point(atof(first), atof(second)), 10, Scalar(0, 255, 255), -1);
+        }
+
+        imwrite(dirOut, srcImage);
+        letter++;
+        srcImage.release();
+    }
 
     srcImage = imread("Images/MojeZdjecie.jpg");
     if (!srcImage.data) {
@@ -24,6 +105,7 @@ int main()
     moveWindow("Piotr Gorkowski", 0, 0);
 
     imshow("Piotr Gorkowski", srcImage);
+
     waitKey(0);
 
 
