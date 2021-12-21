c2c76277688cbe5b0a28a53954a7839406855586
kobas <gorkowski.piotr1@gmail.com>
2021-12-22 00:36:24 +0100

    unity

---
+++ lab5/Assets/Scripts/BallScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    public int balls = 3;
+    public int score = 0;
+    public bool ready = true;
+    public float launchForce = 500.0f;
+    public float bumpForce = 500.0f;
+
+    public void Launch()
+    {
+        if (ready && balls > 0)
+        {
+            GetComponent<Rigidbody>().AddForce(0, launchForce, 0);
+        }
+    }
+}
---
+++ lab5/Assets/Scripts/FlipperController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperController : MonoBehaviour
+{
+    public float torqueForce = 1000.0f;
+    public int spin = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody>().AddTorque(Vector3.forward * spin * torqueForce);
+        }
+    }
+}
